72656e242adbc9872c9b8edc5c4f352b3487fc0b
RX6500XT <wangz9096z@gmail.com>
2025-09-14 14:10:52 +0800

    Update UselessLEDChaos.ino

--- UselessLEDChaos.ino
+++ UselessLEDChaos.ino
@@ -1,30 +1,49 @@
 /*
-This Arduino code is released into the PUBLIC DOMAIN under the Unlicense:
-https://unlicense.org/
-
-Anyone can copy, modify, publish, use, compile, sell, or distribute this software,
-in source or binary form, for any purpose, commercial or non-commercial.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+Chaotic LED Blinker
+Non-blocking version using millis()
+Still proudly useless 😎
 */
 
-int leds[] = {2, 3, 4}; // only 3 pins now
+int leds[] = {2, 3, 4};
 int numLEDs = 3;
+
+unsigned long prevMillis = 0;
+unsigned long interval = 0;
+int currentLED = 0;
+bool ledState = LOW;
 
 void setup() {
   Serial.begin(9600);
-  for(int i = 0; i < numLEDs; i++){
+  for (int i = 0; i < numLEDs; i++) {
     pinMode(leds[i], OUTPUT);
   }
-  pinMode(13, OUTPUT); // built-in LED
+  pinMode(13, OUTPUT);
+  
+  // pick initial random victim LED
+  currentLED = random(0, numLEDs);
+  interval = random(50, 500);
+  prevMillis = millis();
 }
 
 void loop() {
-  int r = random(0, numLEDs);       // pick a random LED from 2,3,4
-  int t = random(50, 500);          // random blink time in ms
-  digitalWrite(leds[r], HIGH);
-  digitalWrite(13, random(0,2));    // built-in LED doing chaotic stuff
-  delay(t);
-  digitalWrite(leds[r], LOW);
-  Serial.println("I am useless ;)"); // pointless chaos messages
+  unsigned long now = millis();
+
+  // check if it's time to toggle chaos
+  if (now - prevMillis >= interval) {
+    prevMillis = now;
+
+    // toggle current LED
+    ledState = !ledState;
+    digitalWrite(leds[currentLED], ledState);
+
+    // built-in LED flips a coin every cycle
+    digitalWrite(13, random(0, 2));
+
+    if (!ledState) {
+      // if we just turned it off, pick a new random LED + new chaos timing
+      currentLED = random(0, numLEDs);
+      interval = random(50, 500);
+      Serial.println("Still useless, but async 😎");
+    }
+  }
 }
